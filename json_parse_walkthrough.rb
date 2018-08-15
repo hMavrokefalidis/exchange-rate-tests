d3ea208cea9b357d7971076e8b45bd89a85c5320
TECH-W74 <cmavrokefalidis@spartaglobal.com>
2018-08-15 10:05:42 +0100

    making sure tests are more readable

--- json_parse_walkthrough.rb
+++ json_parse_walkthrough.rb
@@ -6,6 +6,22 @@ class ParseJSON
   def initialize(json_file)
     @json_file = JSON.parse(File.read(json_file))
   end
+
+  def get_base_value
+    @json_file["base"]
+  end
+
+  def get_date_value
+    @json_file["date"]
+  end
+
+  def get_AUD_value
+    @json_file["rates"]["AUD"]
+  end
+
+  def get_number_of_currencies
+    @json_file["rates"].length
+  end
 end
 
 json = ParseJSON.new("exchange_rates.json")
--- spec/json_spec.rb
+++ spec/json_spec.rb
@@ -1,26 +1,24 @@
 require 'spec_helper.rb'
 
-
-
 describe 'executing test regarding exchange rates compared to euro' do
 
   before(:each) do
     @currency = ParseJSON.new("exchange_rates.json")
   end
   it "should have a base of EUR" do
-    expect(@currency.json_file["base"]).to eq("EUR")
+    expect(@currency.get_base_value).to eq("EUR")
   end
 
   it "should be of the date 2017-07-26" do
-    expect(@currency.json_file["date"]).to eq("2017-07-26")
+    expect(@currency.get_date_value).to eq("2017-07-26")
   end
 
   it "should check if AUD to be less than the value of Euro" do
-    expect(@currency.json_file["rates"]["AUD"]).to be > 1
+    expect(@currency.get_AUD_value).to be > 1
   end
 
   it "Checking if there are 31 currencies being tested" do
-    expect(@currency.json_file["rates"].length).to eq(31)
+    expect(@currency.get_number_of_currencies).to eq(31)
   end
 
 end
